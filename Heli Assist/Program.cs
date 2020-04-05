0af363859619378a14cbfb0da2f0bd441977136e
Computermatronic <computermatronic@gmail.com>
2020-04-06 01:36:40 +1000

    V1.11 Update

--- Heli Assist/Program.cs
+++ Heli Assist/Program.cs
@@ -7,132 +7,72 @@ using System.Collections;
 using System.Linq;
 using System.Text;
 using System;
-using VRage;
 using VRage.Collections;
 using VRage.Game.Components;
-using VRage.Game.ModAPI.Ingame;
+using VRage.Game.GUI.TextPanel;
 using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
 using VRage.Game.ObjectBuilders.Definitions;
 using VRage.Game;
+using VRage;
 using VRageMath;
 
 namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        const string defaultConfigString = "[main]\nblock_group_name=Heli Assist\nmax_pitch=45\nmax_roll=45\nmax_landing_pitch=10\nmax_landing_roll=10\nprecision=16\nstart_mode=flight\nremember_mode=true";
-
         IMyShipController controller;
-
         GyroController gyroController;
-        ThrusterController thrusterController;
+        ThrusterController thrustController;
 
+        //Runtime Variables
         TimeSpan timeSinceLastUpdate;
-        bool updateSuceeded = false;
-        bool isFirstRun = true;
-        string configString;
+        bool updateFinished = false;
+        bool isFirstUpdate = true;
 
-        List<IMyShipController> controllers;
-        List<IMyGyro> gyros;
-        List<IMyThrust> thrusters;
+        //State Variables;
+        string mode;
 
-        bool precisionEnabled = false;
-        bool lateralDampeners = false;
+        bool enableLateralDampening;
+        bool enablePrecisionAim;
 
-        //Config Options
+        //Config Variables
         string blockGroupName = "Heli Assist";
 
-        float maxFlightPitch = 45.0f;
-        float maxFlightRoll = 45.0f;
+        string start_mode = "flight";
+        bool rememberLastMode = true;
+
+        float maxFlightPitch = 30.0f;
+        float maxFlightRoll = 20.0f;
 
         float maxLandingPitch = 10.0f;
         float maxLandingRoll = 10.0f;
 
-        float precisionMultiplier = 16.0f;
+        float precisionAimFactor = 16.0f;
+        float mouseSpeed = 0.5f;
 
-        string mode = "flight";
-        bool rememberMode = true;
+        //Cache Variables
+        List<IMyShipController> controllerCache;
+        List<IMyGyro> gyroCache;
+        List<IMyThrust> thrustCache;
 
+        string configCache;
+
+        static Program program;
 
         public Program()
         {
-            this.controllers = new List<IMyShipController>();
-            this.gyros = new List<IMyGyro>();
-            this.thrusters = new List<IMyThrust>();
+            controllerCache = new List<IMyShipController>();
+            gyroCache = new List<IMyGyro>();
+            thrustCache = new List<IMyThrust>();
 
             timeSinceLastUpdate = TimeSpan.FromSeconds(0);
             Runtime.UpdateFrequency = UpdateFrequency.Update1;
-        }
 
-        string Update()
-        {
-            updateSuceeded = false;
-            try
-            {
-                string configString = Me.CustomData;
-                if (configString.Length == 0) Me.CustomData = configString = defaultConfigString;
+            try { this.Update(); }
+            catch (Exception e) { Echo("Error: " + e.Message); }
 
-                if (configString != this.configString)
-                {
-                    this.configString = configString;
-                    MyIni configIni = new MyIni();
-                    MyIniParseResult parseResult;
-
-                    if (!configIni.TryParse(configString, out parseResult)) throw new Exception("Failed To Read Config: " + parseResult.Error + " on line" + parseResult.LineNo.ToString());
-
-                    maxFlightPitch = (float)configIni.Get("main", "max_pitch").ToDouble(maxFlightPitch);
-                    maxFlightRoll = (float)configIni.Get("main", "max_roll").ToDouble(maxFlightRoll);
-
-                    maxLandingPitch = (float)configIni.Get("main", "max_landing_pitch").ToDouble(maxLandingPitch);
-                    maxLandingRoll = (float)configIni.Get("main", "max_landing_roll").ToDouble(maxLandingRoll);
-
-                    precisionMultiplier = (float)configIni.Get("main", "precision").ToDouble(precisionMultiplier);
-
-                    blockGroupName = configIni.Get("main", "block_group_name").ToString(blockGroupName);
-                    rememberMode = configIni.Get("main", "remember_mode").ToBoolean(rememberMode);
-
-                    if (isFirstRun)
-                    {
-                        mode = configIni.Get("main", "start_mode").ToString(mode);
-                        if (!isValidMode(mode))
-                            throw new Exception("'" + mode + "is not a valid value for start_mode");
-                    }
-                }
-
-                if (isFirstRun && Storage.Length > 0 && rememberMode) mode = Storage;
-
-                var blockGroup = GridTerminalSystem.GetBlockGroupWithName(blockGroupName);
-                if (blockGroup == null) throw new Exception("Could not find block group with name '" + blockGroupName + "'");
-
-                controllers.Clear();
-                blockGroup.GetBlocksOfType<IMyShipController>(controllers);
-                if (!controllers.Any()) throw new Exception("Ship must have atleast one ship controller");
-                foreach (var controller in controllers)
-                {
-                    if (controller.IsMainCockpit) this.controller = controller;
-                }
-                if (this.controller == null) this.controller = controllers.First();
-
-                gyros.Clear();
-                blockGroup.GetBlocksOfType<IMyGyro>(gyros);
-                if (!gyros.Any()) throw new Exception("Ship must have atleast one gyroscope");
-                gyroController = new GyroController(controller, gyros);
-
-                thrusters.Clear();
-                blockGroup.GetBlocksOfType<IMyThrust>(thrusters);
-                if (!thrusters.Any()) throw new Exception("Ship must have atleast one thruster");
-                thrusterController = new ThrusterController(controller, thrusters);
-
-                if (isFirstRun) SwitchToMode(mode);
-
-                updateSuceeded = true;
-                isFirstRun = false;
-            }
-            catch (Exception e)
-            {
-                return "Error: " + e.Message;
-            }
-            return "";
+            program = this;
         }
 
         public void Save()
@@ -140,42 +80,44 @@ namespace IngameScript
             Storage = mode;
         }
 
-
-
         public void Main(string argument, UpdateType updateSource)
         {
             timeSinceLastUpdate += Runtime.TimeSinceLastRun;
 
-            if (isFirstRun || !updateSuceeded || timeSinceLastUpdate > TimeSpan.FromSeconds(10))
+            if (isFirstUpdate || !updateFinished || timeSinceLastUpdate > TimeSpan.FromSeconds(10))
             {
-                Echo(Update());
+                try { this.Update(); }
+                catch (Exception e) { Echo("Error: " + e.Message); }
                 timeSinceLastUpdate = TimeSpan.FromSeconds(0);
                 return;
             }
 
             Echo("Script running, next update: " + (10 - (uint)timeSinceLastUpdate.TotalSeconds).ToString());
             Echo("Current Mode: " + mode);
-            Echo("Precision Aim: " + (precisionEnabled ? "enabled" : "disabled"));
-            Echo("Lateral Dampening: " + (lateralDampeners ? "enabled" : "disabled"));
+            Echo("Precision Aim: " + (enablePrecisionAim ? "enabled" : "disabled"));
+            Echo("Lateral Dampening: " + (enableLateralDampening ? "enabled" : "disabled"));
 
-            var wasd = controller.MoveIndicator;
-            var mouse = new Vector3D(controller.RotationIndicator, controller.RollIndicator * 9);
-
-            if (precisionEnabled) mouse *= 1 / precisionMultiplier;
-
-            if (isValidMode(argument))
+            if (IsValidMode(argument))
                 SwitchToMode(argument);
             else if (argument == "toggle_manual") SwitchToMode(mode == "manual" ? "flight" : "manual");
             else if (argument == "toggle_landing") SwitchToMode(mode == "landing" ? "flight" : "landing");
             else if (argument == "toggle_shutdown") SwitchToMode(mode == "shutdown" ? "flight" : "shutdown");
             else if (argument == "toggle_standby") SwitchToMode(mode == "standby" ? "flight" : "standby");
-            else if (argument == "toggle_precision") precisionEnabled = !precisionEnabled;
-            else if (argument == "toggle_lateral_dampening") lateralDampeners = !lateralDampeners;
+            else if (argument == "toggle_precision") enablePrecisionAim = !enablePrecisionAim;
+            else if (argument == "toggle_lateral_dampening") enableLateralDampening = !enableLateralDampening;
             else if (argument == "update")
             {
-                updateSuceeded = false;
+                updateFinished = false;
                 return;
             }
+
+            var wasd = controller.MoveIndicator;
+            var mouse = new Vector3(controller.RotationIndicator, controller.RollIndicator * 9);
+            var dampeningRotation = gyroController.CalculatePitchRollToAchiveVelocity(Vector3.Zero);
+            var autoStop = controller.DampenersOverride;
+
+            if (enablePrecisionAim) mouse *= 1 / precisionAimFactor;
+            else mouse *= mouseSpeed;
 
             switch (mode)
             {
@@ -183,35 +125,30 @@ namespace IngameScript
                     {
                         var pitch = wasd.Z * maxFlightPitch * degToRad;
                         var roll = wasd.X * maxFlightRoll * degToRad;
+                        dampeningRotation = Vector2.Min(dampeningRotation, new Vector2(maxFlightRoll, maxFlightPitch) * degToRad);
 
-                        var pitchRollToStop = gyroController.CalculatePitchRollToStop(maxFlightPitch * degToRad, maxFlightRoll * degToRad);
-                        var autoStop = controller.DampenersOverride;
+                        if (autoStop && IsZero(roll)) roll = MinAbs(dampeningRotation.X, maxFlightRoll * degToRad);
+                        if (autoStop && IsZero(pitch)) pitch = MinAbs(dampeningRotation.Y, maxFlightPitch * degToRad);
 
-                        if (IsZero(pitch) && autoStop && !lateralDampeners) pitch = (float)pitchRollToStop.Y;
-                        if (IsZero(roll) && autoStop) roll = (float)pitchRollToStop.X;
-
-                        gyroController.SetPitchRoll(pitch, roll, mouse);
-                        thrusterController.SetYAxisThrust(IsZero(wasd.Y) ? thrusterController.CalculateThrustToHover() : 0);
+                        gyroController.SetAngularVelocity(gyroController.CalculateVelocityToAlign(pitch, roll) + mouse);
+                        thrustController.SetYAxisThrust(wasd.Y != 0 ? 0 : thrustController.CalculateThrustToHover());
                         break;
                     }
                 case "landing":
                     {
-                        var pitch = wasd.Z * maxLandingPitch * degToRad;
-                        var roll = wasd.X * maxLandingRoll * degToRad;
+                        var pitch = wasd.Z * maxFlightPitch * degToRad;
+                        var roll = wasd.X * maxFlightRoll * degToRad;
+                        dampeningRotation = Vector2.Min(dampeningRotation, new Vector2(maxLandingRoll, maxLandingPitch) * degToRad);
 
-                        var pitchRollToStop = gyroController.CalculatePitchRollToStop(maxLandingPitch * degToRad, maxLandingRoll * degToRad);
-                        var autoStop = controller.DampenersOverride;
+                        if (autoStop && IsZero(roll)) roll = Math.Min(Math.Max(dampeningRotation.X, -maxLandingRoll), maxLandingRoll);
+                        if (autoStop && IsZero(pitch)) pitch = Math.Min(Math.Max(dampeningRotation.Y, -maxLandingPitch), maxLandingPitch);
 
-                        if (IsZero(pitch) && autoStop && !lateralDampeners) pitch = (float)pitchRollToStop.Y;
-                        if (IsZero(roll) && autoStop) roll = (float)pitchRollToStop.X;
-
-                        gyroController.SetPitchRoll(pitch, roll, mouse);
-                        thrusterController.SetYAxisThrust(IsZero(wasd.Y) ? thrusterController.CalculateThrustToHover() : 0);
+                        gyroController.SetAngularVelocity(gyroController.CalculateVelocityToAlign(pitch, roll) + mouse);
+                        thrustController.SetYAxisThrust(wasd.Y != 0 ? 0 : thrustController.CalculateThrustToHover());
                         break;
                     }
                 case "manual":
-                    thrusterController.SetYAxisThrust(IsZero(wasd.Y) ? thrusterController.CalculateThrustToHover() : 0);
-                    gyroController.SetVelocity(mouse);
+                    gyroController.SetAngularVelocity(mouse); thrustController.SetYAxisThrust(wasd.Y != 0 ? 0 : thrustController.CalculateThrustToHover());
                     break;
                 case "shutdown":
                     break;
@@ -222,43 +159,102 @@ namespace IngameScript
 
         void SwitchToMode(string mode)
         {
+            if (!IsValidMode(mode)) return;
             switch (mode)
             {
                 case "flight":
                     gyroController.SetEnabled(true);
-                    thrusterController.SetEnabled(true);
-                    gyroController.SetGyroOverride(true);
+                    thrustController.SetEnabled(true);
+                    gyroController.SetOverride(true);
                     break;
                 case "landing":
                     gyroController.SetEnabled(true);
-                    thrusterController.SetEnabled(true);
-                    gyroController.SetGyroOverride(true);
+                    thrustController.SetEnabled(true);
+                    gyroController.SetOverride(true);
                     controller.DampenersOverride = true;
                     break;
                 case "manual":
                     gyroController.SetEnabled(true);
-                    thrusterController.SetEnabled(true);
-                    gyroController.SetGyroOverride(true);
+                    thrustController.SetEnabled(true);
+                    gyroController.SetOverride(true);
                     break;
                 case "shutdown":
                     gyroController.SetEnabled(false);
-                    thrusterController.SetEnabled(false);
+                    thrustController.SetEnabled(false);
                     break;
                 case "standby":
                     gyroController.SetEnabled(true);
-                    thrusterController.SetEnabled(true);
-                    gyroController.SetGyroOverride(false);
-                    thrusterController.SetYAxisThrust(0);
+                    thrustController.SetEnabled(true);
+                    gyroController.SetOverride(false);
+                    thrustController.SetYAxisThrust(0);
                     break;
             }
-            if (isValidMode(mode)) this.mode = mode;
-            precisionEnabled = false;
-            lateralDampeners = false;
+            this.mode = mode;
+            enablePrecisionAim = false;
+            enableLateralDampening = false;
         }
 
-        bool isValidMode(string mode)
+        bool IsValidMode(string mode)
         {
             return mode == "flight" || mode == "landing" || mode == "manual" || mode == "shutdown" || mode == "standby";
+        }
+
+        public void Update()
+        {
+            if (isFirstUpdate || configCache != Me.CustomData)
+            {
+                configCache = Me.CustomData;
+                MyIni configIni = new MyIni();
+                MyIniParseResult parseResult;
+
+                if (!configIni.TryParse(configCache, out parseResult))
+                    throw new Exception("Failed To Read Config: " + parseResult.Error + " on line" + parseResult.LineNo.ToString());
+
+                blockGroupName = configIni.Get("main", "block_group_name").ToString(blockGroupName);
+
+                start_mode = configIni.Get("main", "start_mode").ToString(start_mode);
+                rememberLastMode = configIni.Get("main", "remember_mode").ToBoolean(rememberLastMode);
+
+                maxFlightPitch = (float)configIni.Get("main", "max_pitch").ToDouble(maxFlightPitch);
+                maxFlightRoll = (float)configIni.Get("main", "max_roll").ToDouble(maxFlightRoll);
+
+                maxLandingPitch = (float)configIni.Get("main", "max_landing_pitch").ToDouble(maxLandingPitch);
+                maxLandingRoll = (float)configIni.Get("main", "max_landing_roll").ToDouble(maxLandingRoll);
+
+                precisionAimFactor = (float)configIni.Get("main", "precision").ToDouble(precisionAimFactor);
+            }
+
+            var blockGroup = GridTerminalSystem.GetBlockGroupWithName(blockGroupName);
+            if (blockGroup == null) throw new Exception("Could not find block group with name '" + blockGroupName + "'");
+
+            controllerCache.Clear();
+            blockGroup.GetBlocksOfType<IMyShipController>(controllerCache);
+            if (!controllerCache.Any()) throw new Exception("Ship must have at least one ship controller");
+            foreach (var controller in controllerCache)
+            {
+                if (controller.IsMainCockpit) this.controller = controller;
+            }
+            if (this.controller == null) this.controller = controllerCache.First();
+
+            gyroCache.Clear();
+            blockGroup.GetBlocksOfType<IMyGyro>(gyroCache);
+            if (!gyroCache.Any()) throw new Exception("Ship must have atleast one gyroscope");
+
+            thrustCache.Clear();
+            blockGroup.GetBlocksOfType<IMyThrust>(thrustCache);
+            if (!thrustCache.Any()) throw new Exception("Ship must have atleast one thruster");
+
+            if (thrustController == null) thrustController = new ThrusterController(controller, thrustCache);
+            else thrustController.Update(controller, thrustCache);
+
+            if (gyroController == null) gyroController = new GyroController(controller, gyroCache);
+            else gyroController.Update(controller, gyroCache);
+
+            if (isFirstUpdate && rememberLastMode && IsValidMode(Storage)) SwitchToMode(Storage);
+            else if (isFirstUpdate) SwitchToMode(start_mode);
+
+            isFirstUpdate = false;
+            updateFinished = true;
         }
     }
 }
